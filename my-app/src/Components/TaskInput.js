b2f981b3d86c10634a8e410896b5df5df16a43ff
Eric Larwa <ericlarwa@erics-air.intra.ecu.edu>
2024-03-15 14:00:04 -0400

    stututut

--- my-app/src/Components/TaskInput.js
+++ my-app/src/Components/TaskInput.js
@@ -6,9 +6,8 @@ import HeaderArea from './HeaderArea'
 const Input = () => {
 
     const [columns, setColumns] = useState([
-        [{ type: 'header', content: 'To-do' }],
-        [{ type: 'header', content: 'Progress' }],
-        [{ type: 'header', content: 'Done' }]
+        [{ type: 'header', content: 'Column 1' }],
+        [{ type: 'card', content: 'Card 1' }]
       ]);
 
     const addCard = (columnIndex) => {
@@ -16,50 +15,42 @@ const Input = () => {
         newColumns[columnIndex].push({ type: 'card', content: '' });
         setColumns(newColumns);
     };
-    
+
     const handleChange = (event, columnIndex, cardIndex) => {
         const newColumns = [...columns];
         newColumns[columnIndex][cardIndex].content = event.target.value;
         setColumns(newColumns);
     };
-
-    const handleDeleteCard = (columnIndex) => (cardIndex) =>{
-        console.log('Deleteing card:', columnIndex, cardIndex);
-        const newColumns =[...columns];
-        newColumns[columnIndex].splice(cardIndex, 1);
-        console.log("New Columns:", newColumns);
-        setColumns(newColumns);
-    }
     
       return (
         <div className="container">
-            {columns.map((column, columnIndex) => (
-                <div key={columnIndex} className="column">
-                    {column.map((item, cardIndex) => (
-                        <div key={cardIndex}>
-                            {item.type === 'header' ? (
-                                <HeaderArea name={item.content} showDeleteButton={false}/>
-                            ) : (
-                                <div>
-                                    <input
-                                        type="text"
-                                        className="card"
-                                        placeholder={`Card ${cardIndex + 1}`}
-                                        value={item.content}
-                                        onChange={(event) => handleChange(event, columnIndex, cardIndex)}
-                                    />
-                                    <Button variant="danger" onClick={() => handleDeleteCard(columnIndex)(cardIndex)}>
-                                        <FaTrash />
-                                    </Button>
-                                </div>
-                            )}
-                        </div>
-                    ))}
-                    <button onClick={() => addCard(columnIndex)}>Add Card</button>
+        {columns.map((column, columnIndex) => (
+            <div key={columnIndex} className="column">
+            {column.map((item, cardIndex) => (
+                <div key={cardIndex}>
+                {item.type === 'header' ? (
+                    <input
+                    type="text"
+                    className="header"
+                    placeholder={item.content}
+                    />
+                ) : (
+                    <input
+                    type="text"
+                    className="card"
+                    placeholder={`Card ${cardIndex + 1}`}
+                    value={item.content}
+                    onChange={(event) => handleChange(event, columnIndex, cardIndex)}
+                    />
+
+                )}
                 </div>
             ))}
+            <button onClick={() => addCard(columnIndex)}>Add Card</button>
+            </div>
+        ))}
         </div>
       );
-};
+}
 
 export default Input;
